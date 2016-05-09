eb7cc6960cdb5f27d071ff88c1e63418ead2a6a7
focusworld <rezaabsy@yahoo.com>
2016-05-09 16:56:27 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,7 +224,8 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-    "admin"
+    "admin",
+    "addplug"
     },
     sudo_users = {157187288,202640295,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
